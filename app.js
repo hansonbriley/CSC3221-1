82c4036fed1807d5ca50a613c2c0a79c14e77cd8
Briley Hanson <hansonb7@spu.edu>
2020-09-20 16:20:29 -0700

    updated app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 5000;
 const server = http.createServer((request, response) => {
 	response.statusCode = 200;
 	response.setHeader('Content-Type', 'text/plain');
-	response.end("Welcome to my Web Application\n\nMy name is: Briley Hanson');
+	response.end("Welcome to my Web Application\n\nMy name is: Briley Hanson");
 });
 
 server.listen(port, (err) => {
